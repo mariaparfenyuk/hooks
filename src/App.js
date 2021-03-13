d2441324afa7bac09efb4b09e898b6ab9317fa22
Maria Parfenyuk <maria_parfenyuk@epam.com>
2021-03-13 21:50:45 +0300

    useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [type, setType] = useState('users')
+  const [data, setData] = useState([])
+
+  
+
+  // useEffect(() => {
+  //   console.log('render');
+  // })
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+  .then(response => response.json())
+  .then(json => setData(json))
+  }, [type])
+
   return (
     <div>
-      
+      <h1>Ресурс: {type}</h1>
+
+      <button onClick={() => setType('users')}>Пользователи</button>
+      <button onClick={() => setType('todos')}>Todo</button>
+      <button onClick={() => setType('posts')}>Посты</button>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
+// https://www.youtube.com/watch?v=9KJxaFHotqI 32:35
 
 export default App;
