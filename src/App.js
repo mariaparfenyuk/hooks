8822701cfa331eae2fa7fab0b66992c5ad34dc07
Maria Parfenyuk <maria_parfenyuk@epam.com>
2021-03-14 19:24:24 +0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+
+// let renderCount = 1
 
 function App() {
-  const [type, setType] = useState('users')
-  const [data, setData] = useState([])
-
-  
-
-  // useEffect(() => {
-  //   console.log('render');
-  // })
+  // const [renderCount, setRenderCount] = useState(1)
+  const [value, setValue] = useState('initial')
+  const renderCount = useRef(1)
+  const inputRef = useRef(null)
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-  .then(response => response.json())
-  .then(json => setData(json))
-  }, [type])
+    renderCount.current++
+    console.log(inputRef.current.value);
+  })
+
+  // https://www.youtube.com/watch?v=9KJxaFHotqI 45:23
 
   return (
     <div>
-      <h1>Ресурс: {type}</h1>
-
-      <button onClick={() => setType('users')}>Пользователи</button>
-      <button onClick={() => setType('todos')}>Todo</button>
-      <button onClick={() => setType('posts')}>Посты</button>
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>Количесто рендеров: {renderCount.current}</h1>
+      <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value} />
+      <button className="btn btn-success" ></button>
     </div>
   );
 }
-// https://www.youtube.com/watch?v=9KJxaFHotqI 32:35
+
 
 export default App;
